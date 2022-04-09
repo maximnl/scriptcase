1388b81cd4b88d1bcee10dc7feb128f9ee9216dc
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-04-09 15:54:12 +0200

    Update general_php.php

--- general_php.php
+++ general_php.php
@@ -1,2 +1,17 @@
 // short version of if
 [usr_priv_admin] 	= ({rs[0][0]} == 'Y') ? TRUE : FALSE;
+
+
+
+// USING COOKIE
+?>
+<script> document.cookie = {"myJavascriptVar" : window.innerWidth} </script>
+<?php
+
+$SG_Width= $_COOKIE['myJavascriptVar'];
+echo $SG_Width;
+if ($SG_Width >= 1000) {
+	echo "mayor<br>";
+} else {
+	echo "menor<br>";
+}
